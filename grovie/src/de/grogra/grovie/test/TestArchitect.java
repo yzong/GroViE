85f7a95e595670c1bf63bef4b93712c9fd1e4c64
yong <yonggithub@gmail.com>
2013-11-19 10:47:45 +0100

    architect tests

--- grovie/src/de/grogra/grovie/test/TestArchitect.java
+++ grovie/src/de/grogra/grovie/test/TestArchitect.java
@@ -13,8 +13,14 @@ import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.awt.GLCanvas;
 
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import de.grogra.grovie.test.TestDB.RelTypes;
 
 
 
@@ -26,92 +32,192 @@ public class TestArchitect {
 
 	public static GraphDatabaseService graphDb;
 	public static String DB_PATH = "C:\\Users\\yong\\db";
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		System.out.println("TestArchitect Main");
-		
+
 		System.out.println("Test Neo4j");
+		createDB();
 		testDB();
 		System.out.println("Test Neo4j End");
-		
-		
+
+		System.out.println("Test db access- 1. same thread - begin");
+		testAccess1();
+		System.out.println("Test db access- 1. same thread - end");
+
 		System.out.println("Test JOGL");
-		testGL();
+		//testGLAWT();
+		
 		System.out.println("Test JOGL End");
+
+		while(true)
+		{
+
+		}
+	}
+	
+	
+	public TestArchitect()
+	{
+		System.out.println("TestArchitect Main");
+
+		System.out.println("Test Neo4j");
+		createDB();
+		testDB();
+		System.out.println("Test Neo4j End");
+
+		System.out.println("Test db access- 1. same thread - begin");
+		testAccess1();
+		System.out.println("Test db access- 1. same thread - end");
+
+		System.out.println("Test JOGL");
+		testGLAWT();
+		System.out.println("Test JOGL End");
+
+		while(true)
+		{
+
+		}
+	}
+
+	private static void createDB()
+	{
+		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+		registerShutdownHook( graphDb );
 	}
 
 	private static void registerShutdownHook( final GraphDatabaseService graphDb )
 	{
-	    // Registers a shutdown hook for the Neo4j instance so that it
-	    // shuts down nicely when the VM exits (even if you "Ctrl-C" the
-	    // running application).
-	    Runtime.getRuntime().addShutdownHook( new Thread()
-	    {
-	        @Override
-	        public void run()
-	        {
-	            graphDb.shutdown();
-	        }
-	    } );
+		// Registers a shutdown hook for the Neo4j instance so that it
+		// shuts down nicely when the VM exits (even if you "Ctrl-C" the
+		// running application).
+		Runtime.getRuntime().addShutdownHook( new Thread()
+		{
+			@Override
+			public void run()
+			{
+				graphDb.shutdown();
+			}
+		} );
 	}
-	
+
 	private static void testDB()
 	{
 		try{
-			graphDb = new TestDB().getDB();
+			new TestDB(graphDb);
 		}catch(Exception e)
 		{
 			System.out.println("testDB failed.");
 		}
 	}
 	
-	private static void testGL()
+	private static void testGLAWT()
 	{
 		try{
 			GLProfile glprofile = GLProfile.getDefault();
-	        GLCapabilities glcapabilities = new GLCapabilities( glprofile );
-	        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
-	
-	        glcanvas.addGLEventListener( new GLEventListener() {
-	            
-	            @Override
-	            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
-	                OneTriangle.setup( glautodrawable.getGL().getGL2(), width, height );
-	            }
-	            
-	            @Override
-	            public void init( GLAutoDrawable glautodrawable ) {
-	            }
-	            
-	            @Override
-	            public void dispose( GLAutoDrawable glautodrawable ) {
-	            }
-	            
-	            @Override
-	            public void display( GLAutoDrawable glautodrawable ) {
-	                OneTriangle.render( glautodrawable.getGL().getGL2(), glautodrawable.getWidth(), glautodrawable.getHeight() );
-	            }
-	        });
-	
-	        final Frame frame = new Frame( "One Triangle AWT" );
-	        frame.add( glcanvas );
-	        frame.addWindowListener( new WindowAdapter() {
-	            public void windowClosing( WindowEvent windowevent ) {
-	                frame.remove( glcanvas );
-	                frame.dispose();
-	                System.exit( 0 );
-	            }
-	        });
-	
-	        frame.setSize( 640, 480 );
-	        frame.setVisible( true );
+			GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+			final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+
+			glcanvas.addGLEventListener( new GLEventListener() {
+
+				@Override
+				public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+					OneTriangleAWT.setup( glautodrawable.getGL().getGL2(), width, height );
+				}
+
+				@Override
+				public void init( GLAutoDrawable glautodrawable ) {
+				}
+
+				@Override
+				public void dispose( GLAutoDrawable glautodrawable ) {
+				}
+
+				@Override
+				public void display( GLAutoDrawable glautodrawable ) {
+					OneTriangleAWT.render( glautodrawable.getGL().getGL2(), glautodrawable.getWidth(), glautodrawable.getHeight() );
+				}
+			});
+
+			final Frame frame = new Frame( "One Triangle AWT" );
+			frame.add( glcanvas );
+			frame.addWindowListener( new WindowAdapter() {
+				public void windowClosing( WindowEvent windowevent ) {
+					frame.remove( glcanvas );
+					frame.dispose();
+					System.exit( 0 );
+				}
+			});
+
+			frame.setSize( 640, 480 );
+			frame.setVisible( true );
 		}
 		catch(Exception e)
 		{
 			System.out.println("testGL failed.");
+		}
+	}
+
+	/**
+	 * Test database access from same thread
+	 */
+	private static void testAccess1()
+	{
+		for(int i=0; i<100; ++i)
+		{
+			testAccess1Internal();
+		}
+	}
+
+	private static synchronized void testAccess1Internal()
+	{
+		Node firstNode;
+		Node secondNode;
+		Relationship relationship;
+		
+		//create nodes and print data
+		Transaction tx = graphDb.beginTx();
+		try
+		{
+
+			firstNode = graphDb.createNode();
+			firstNode.setProperty( "message", "Hello 1, " );
+			secondNode = graphDb.createNode();
+			secondNode.setProperty( "message", "World!" );
+
+			relationship = firstNode.createRelationshipTo( secondNode, RelTypes.KNOWS );
+			relationship.setProperty( "message", "brave Neo4j " );
+
+			tx.success();
+
+			System.out.print( firstNode.getProperty( "message" ) );
+			System.out.print( relationship.getProperty( "message" ) );
+			System.out.print( secondNode.getProperty( "message" ) );
+			System.out.println();
+		}
+		finally
+		{
+			tx.finish();
+		}
+
+		//delete nodes
+		tx = graphDb.beginTx();
+		try
+		{
+
+			firstNode.getSingleRelationship( RelTypes.KNOWS, Direction.OUTGOING ).delete();
+			firstNode.delete();
+			secondNode.delete();
+
+			tx.success();
+		}
+		finally
+		{
+			tx.finish();
 		}
 	}
 }
--- grovie/src/de/grogra/grovie/test/TestDB.java
+++ grovie/src/de/grogra/grovie/test/TestDB.java
@@ -1,46 +1,149 @@
 package de.grogra.grovie.test;
 
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class TestDB implements Runnable{
 
 	Thread t;
-	String DB_PATH = "C:\\Users\\yong\\db";
-	GraphDatabaseService graphDb;
-			
-	public TestDB()
-	{
-		 // Create a new, second thread
-	      t = new Thread(this, "NeoTest DB Thread");
-	      System.out.println("NeoTest DB thread: " + t);
-	      t.start(); // Start the thread
-	}
 	
+	GraphDatabaseService graphDb;
+
+	/*private final Object lock = new Object();
+	private volatile boolean suspend = false , stopped = false;*/
+
+	enum RelTypes implements RelationshipType
+	{
+		CONTAINED_IN, KNOWS
+	}
+
+	public TestDB(GraphDatabaseService db)
+	{
+		graphDb = db;
+
+		// Create a new, second thread
+		t = new Thread(this, "NeoTest DB Thread");
+		System.out.println("NeoTest DB thread: " + t);
+		t.start(); // Start the thread
+	}
+
 	@Override
 	public void run() {
-		
-		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
-		registerShutdownHook( graphDb );
+
+		//test neo4j access - 2. other thread
+		System.out.println("Test db access- 2.other thread - begin");
+		testAccess2();
+		System.out.println("Test db access- 2.other thread - end");
+
+		/*
+		while(!stopped){
+			while (!suspend){
+				// do work
+			}
+			synchronized (lock){
+				try {
+					lock.wait();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					return;
+				}
+			}
+		}*/
 	}
-	
-	public GraphDatabaseService getDB()
+
+	/*
+	public void suspend(){
+		suspend = true;
+	}
+	public void stop(){
+		suspend = true;stopped = true;
+		synchronized (lock){
+			lock.notifyAll();
+		}
+	}
+
+	public void resume(){
+		suspend = false;
+		synchronized (lock){
+			lock.notifyAll();
+		}
+	}
+	*/
+
+	private void testAccess2()
 	{
-		return graphDb;
+		for(int i=0; i<100; ++i)
+		{
+			testAccess2Internal();
+		}
 	}
-	
+
+	private synchronized void testAccess2Internal()
+	{
+		Node firstNode;
+		Node secondNode;
+		Relationship relationship;
+		
+		//create nodes and print data
+		Transaction tx = graphDb.beginTx();
+		try
+		{
+
+			firstNode = graphDb.createNode();
+			firstNode.setProperty( "message", "Hello 2, " );
+			secondNode = graphDb.createNode();
+			secondNode.setProperty( "message", "World!" );
+
+			relationship = firstNode.createRelationshipTo( secondNode, RelTypes.KNOWS );
+			relationship.setProperty( "message", "brave Neo4j " );
+
+			tx.success();
+
+			System.out.print( firstNode.getProperty( "message" ) );
+			System.out.print( relationship.getProperty( "message" ) );
+			System.out.print( secondNode.getProperty( "message" ) );
+			System.out.println();
+		}
+		finally
+		{
+			tx.finish();
+		}
+
+		//delete nodes
+		tx = graphDb.beginTx();
+		try
+		{
+
+			firstNode.getSingleRelationship( RelTypes.KNOWS, Direction.OUTGOING ).delete();
+			firstNode.delete();
+			secondNode.delete();
+
+			tx.success();
+		}
+		finally
+		{
+			tx.finish();
+		}
+	}
+
+	/*
 	private static void registerShutdownHook( final GraphDatabaseService graphDb )
 	{
-	    // Registers a shutdown hook for the Neo4j instance so that it
-	    // shuts down nicely when the VM exits (even if you "Ctrl-C" the
-	    // running application).
-	    Runtime.getRuntime().addShutdownHook( new Thread()
-	    {
-	        @Override
-	        public void run()
-	        {
-	            graphDb.shutdown();
-	        }
-	    } );
-	}
+		// Registers a shutdown hook for the Neo4j instance so that it
+		// shuts down nicely when the VM exits (even if you "Ctrl-C" the
+		// running application).
+		Runtime.getRuntime().addShutdownHook( new Thread()
+		{
+			@Override
+			public void run()
+			{
+				graphDb.shutdown();
+			}
+		} );
+	}*/
 }
